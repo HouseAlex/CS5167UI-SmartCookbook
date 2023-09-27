ee7f317445f23d6791ee3a978d40625b92705f17
HouseAlex <houseah@mail.uc.edu>
2023-09-27 13:59:13 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+// src/App.js
+
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Title from './components/Title';
+
+function App() {
+  return (
+      <div>
+        <Title/>
+      </div>
+  );
+}
+
+// Friday React Activity
+/*
+1.
+let addTwoNumbers = (a, b) => a + b;
+
+2.
+let strLength = (myStr) => {
+  myStr.Legnth < 10 ? "Short" : "Long"
+}
+
+3.
+if a is greater than b: log a in console
+else: log b in console
+
+*/
+
+export default App;
